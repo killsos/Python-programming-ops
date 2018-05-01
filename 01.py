bfef5dec926a93edbc5cb1aef35f1740e003ddff
killsos <killsos.ql@gmail.com>
2018-05-02 01:54:58 +0800

    add

---
+++ 01.py
@@ -0,0 +1,11 @@
+x  = 1;
+
+y =  x;
+
+print id(x), id(y)
+
+y += 1;
+
+print x, y
+
+print id(x), id(y)
---
+++ 02.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+x = 1
+
+y = 1
+
+z = x
+
+if x is z:
+  print "is"
+else:
+  print "is 成员访问符号"
+
+if (x is y):
+  print "is not"
+else:
+  print "is not 成员访问符号"
